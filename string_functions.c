7748c56a8c4b08b4c3f14d2717722af2817751fb
César Guadarrama <cesarguadarrama@icloud.com>
2017-05-09 02:13:25 -0500

    Moved functions from main

--- string_functions.c
+++ string_functions.c
@@ -23,3 +23,31 @@ size_t read_line(char * string, int size, FILE * file_ptr) {
 
     return length;
 }
+
+/*
+Read the size of the file indicated in the header
+*/
+void read_size(int * columns, int * rows, FILE * file_pointer) {
+  char line[LINE_SIZE];
+  int temp_columns;
+  int temp_rows;
+
+  read_line(line, LINE_SIZE, file_pointer);
+  sscanf(line, "%d %d", &temp_columns, &temp_rows);
+
+  *columns = temp_columns;
+  *rows = temp_rows;
+}
+
+/*
+– Function to read the context of a txt initial state file and store it on a matrix –
+*/
+void read_content(int * columns, int * rows, FILE * file_pointer, int ** matrix) {
+  int temp_int;
+  for (int i = 0; i < *rows; i++) {
+    for (int j = 0; j < *columns; j++) {
+      fscanf(file_pointer, "%d", &temp_int);
+      matrix[j][i] = temp_int;
+    }
+  }
+}
